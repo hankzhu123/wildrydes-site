132ee802f8c01998b65cbf4613b4ca4f9df5c732
Chen Zhu <czhu2@washcoll.edu>
2025-08-26 15:30:00 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_e3nPudO7D', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '9unkg8mm5ur5j913r2n9a96oc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
